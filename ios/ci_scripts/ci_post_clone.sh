eb6e31f33f9c9b354be82ad4e88b09d030e4a219
YudizAndroidVarshil <varshil.s@yudiz.com>
2025-03-07 18:24:31 +0530

    command added in script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,3 +9,4 @@ flutter clean
 flutter pub get
 brew install cocoapods
 pod install
+xcodebuild archive -workspace /Volumes/workspace/repository/ios/Runner.xcworkspace -scheme Runner -destination generic/platform=iOS -archivePath /Volumes/workspace/build.xcarchive -derivedDataPath /Volumes/workspace/DerivedData -resultBundleVersion 3 -resultBundlePath /Volumes/workspace/resultbundle.xcresult -resultStreamPath /Volumes/workspace/tmp/resultBundleStream204cb033-ad3d-47db-aaca-c2fe1956a617.json -IDEPostProgressNotifications=YES CODE_SIGN_IDENTITY=- AD_HOC_CODE_SIGNING_ALLOWED=YES CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM=XGZ34PKRQ2 COMPILER_INDEX_STORE_ENABLE=NO -hideShellScriptEnvironment
